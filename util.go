0ba2852ffaf9832c564c35d68b3269cb1e374e72
Georgy <gstarikov@gmail.com>
2020-12-15 00:41:03 +0300

    add uint types

--- util.go
+++ util.go
@@ -83,7 +83,8 @@ func getGraphType(tipe reflect.Type) Output {
 	switch kind {
 	case reflect.String:
 		return String
-	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		return Int
 	case reflect.Float32, reflect.Float64:
 		return Float
